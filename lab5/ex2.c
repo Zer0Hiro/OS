79348b1cc0df76f4eb618fa40f6177781d595dcf
Zer0Hiro <chibvk43@gmail.com>
2026-05-12 22:52:10 +0300

    lab5

--- lab5/ex2.c
+++ lab5/ex2.c
@@ -1,10 +1,10 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
 
-void *count();
-void *logs();
+void* count();
+void* logs();
 
 unsigned long int counter = 0;
 
@@ -12,24 +12,35 @@ int main()
 {
     pthread_t tid[2];
 
-    pthread_create(&tid[0], NULL, count, NULL);
-    pthread_create(&tid[1], NULL, logs, NULL);
-
-    pthread_join(tid[0], NULL);
-    pthread_join(tid[1], NULL);
-}
-
-void *count()
-{
-    while (1)
-        counter++;
-}
-
-void *logs()
-{
-    while (1)
+    // Create threads
+    if (pthread_create(&tid[0], NULL, count, NULL) != 0)
     {
-        printf("%lu", counter);
+        perror("Thread th1 failed");
+        exit(1);
+    }
+
+    if (pthread_create(&tid[1], NULL, logs, NULL) != 0)
+    {
+        perror("Thread th2 failed");
+        exit(1);
+    }
+
+    // Wait for logs thread
+    pthread_join(tid[1], NULL);
+    return 0;
+}
+
+void* count()
+{
+    while (1) counter++;
+}
+
+// Prints every 100ms counter
+void* logs()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%lu\n", counter);
         usleep(100000);
     }
 }
---
+++ lab5/ex3.c
@@ -0,0 +1,67 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define N 10
+
+void* search(void* pos);
+
+int arr[N];
+int num;
+pthread_t tid[2];
+int flag = 0;
+
+int main()
+{
+    // random seed for rand()
+    srand(time(NULL));
+
+    // Fill arr with rand numbers 0-100
+    for (int i = 0; i < N; i++) arr[i] = rand() % 101;
+
+    // Get number
+    printf("input: ");
+    scanf("%d", &num);
+
+    // Create threads
+    int posEven = 0;
+    if (pthread_create(&tid[0], NULL, search, &posEven) != 0)
+    {
+        perror("Thread failed");
+        exit(1);
+    }
+
+    int posOdd = 1;
+    if (pthread_create(&tid[1], NULL, search, &posOdd) != 0)
+    {
+        perror("Thread failed");
+        exit(1);
+    }
+
+    // Wait for threads
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+
+    if (flag)
+    {
+        printf("%d is part of the matrix\n", num);
+        return 0;
+    }
+    printf("%d not in matrix\n", num);
+    return 1;
+}
+
+void* search(void* pos)
+{
+    for (int i = *(int*)pos; i < N; i += 2)
+    {
+        // Check if other thread found
+        if (flag) return 0;
+
+        if (arr[i] == num)
+        {
+            flag = 1;
+            return 0;
+        }
+    }
+}
